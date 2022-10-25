04768f6544c10d4151add22265be5bc9d6c3406c
Lovro Boric <lovro.boric@gmail.com>
2022-10-25 21:14:33 +0200

    created searcHParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Riki" animal="dog" breed="Pinscher" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
+
+
     </div>
   );
 };
