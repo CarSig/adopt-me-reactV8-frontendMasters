f21cd4b01253afffdad18dff10fec3bd0ab95ced
Lovro Boric <lovro.boric@gmail.com>
2022-10-25 23:18:22 +0200

    uncontrolled form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,49 +1,51 @@
-
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
-import { useState, useEffect } from "react";
-
+import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("")
-    const [animal, setAnimal] = useState("")
-    const [breed, setBreed] = useState("");
-    const [pets, setPets] = useState([])
-    const [breeds] = useBreedList(animal)
+    const [requestParams, setRequestParams] = useState({
+        location: "",
+        animal: "",
+        breed: "",
+    });
+    const [animal, setAnimal] = useState("");
+    const [breeds] = useBreedList(animal);
 
-    useEffect(() => { requestPets() }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-    async function requestPets() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-        );
-        const json = await res.json();
-
-        setPets(json.pets);
-    }
+    const results = useQuery(["search", requestParams], fetchSearch);
+    const pets = results?.data?.pets ?? [];
 
     return (
         <div className="search-params">
-            <form onSubmit={(e) => { e.preventDefault(); requestPets() }
-            }>
-                <label htmlFor="location">Location
-                    <input id="location" value={location} placeholder="location" onChange={(e) => { setLocation(e.target.value) }} />
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    const formData = new FormData(e.target);
+                    const obj = {
+                        animal: formData.get("animal") ?? "",
+                        breed: formData.get("breed") ?? "",
+                        location: formData.get("location") ?? "",
+                    };
+                    setRequestParams(obj);
+                }}
+            >
+                <label htmlFor="location">
+                    Location
+                    <input id="location" name="location" placeholder="Location" />
                 </label>
-
 
                 <label htmlFor="animal">
                     Animal
                     <select
                         id="animal"
-                        value={animal}
+                        name="animal"
                         onChange={(e) => {
                             setAnimal(e.target.value);
-                            setBreed("");
                         }}
                         onBlur={(e) => {
                             setAnimal(e.target.value);
-                            setBreed("");
                         }}
                     >
                         <option />
@@ -57,13 +59,7 @@ const SearchParams = () => {
 
                 <label htmlFor="breed">
                     Breed
-                    <select
-                        disabled={!breeds.length}
-                        id="breed"
-                        value={breed}
-                        onChange={(e) => setBreed(e.target.value)}
-                        onBlur={(e) => setBreed(e.target.value)}
-                    >
+                    <select disabled={!breeds.length} id="breed" name="breed">
                         <option />
                         {breeds.map((breed) => (
                             <option key={breed} value={breed}>
@@ -72,16 +68,12 @@ const SearchParams = () => {
                         ))}
                     </select>
                 </label>
+
                 <button>Submit</button>
-
             </form>
-
             <Results pets={pets} />
-
-
-
         </div>
-    )
-}
+    );
+};
 
-export default SearchParams
+export default SearchParams;
