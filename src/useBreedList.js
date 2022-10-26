423293614c45e0244dd4d13d04206a2d88810ab8
Lovro Boric <lovro.boric@gmail.com>
2022-10-26 17:29:44 +0200

    modal(portals and refs)

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchBreedList";
+// import fetchBreedList from "./fetchBreedList";
 import fetchFactory from "./fetchFactory";
 
 
